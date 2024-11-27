05e38e84ab04d127117d0540c6422b26ca36d6d0
Ofer Bochan <83212457+obochan-rh@users.noreply.github.com>
2024-11-27 13:06:58 +0200

    update dast nrop apiUrl (#21)

--- tests/dast/rapid-ptp/create_rapidast_configmap.sh
+++ tests/dast/rapid-ptp/create_rapidast_configmap.sh
@@ -31,7 +31,7 @@ data:
       zap:
         apiScan:
           apis:
-            apiUrl: "https://kubernetes.default.svc/openapi/v3/apis/ptp.openshift.io/v1alpha1"
+            apiUrl: "https://kubernetes.default.svc/openapi/v3/apis/ptp.openshift.io/v1"
         passiveScan:
           disabledRules: "2,10015,10027,10096,10024,10054"
         activeScan:
